188c2e3202ee0c940d6644c6c49fe122a746f62c
Mahmoud BIDRY <mahmoud.bidrt11@gmail.com>
2022-12-15 13:47:05 +0100

    git engine

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -10,12 +10,12 @@ export class User extends Component {
         <div className="card">
             <img className="card-img-top" src={avatar_url} />
             <div className="card-body">
-            <h4 className="card-title">{login}</h4>
-            <p className="card-text">
-                <a href={"/users/"+login} className='btn btn-success'>Show more</a>
-                <a href={repos_url} className='btn btn-warning'>Repository</a>
-                <a href={html_url} className='btn btn-primary'>View profil</a>
-            </p>
+              <h4 className="card-title">{login}</h4>
+              <p className="card-text">
+                  <a href={"/users/"+login} className='btn btn-success'>Show more</a>
+                  <a href={repos_url} className='btn btn-warning'>Repository</a>
+                  <a href={html_url} className='btn btn-primary'>View profil</a>
+              </p>
             </div>
         </div> 
     )
